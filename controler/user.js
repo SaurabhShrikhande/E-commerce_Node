bf9d799086cccf6fff7b3d384977e73b20ee607b
Saurabh Shrikhande <saurabhshrikhande@200gmail.com>
2024-03-30 23:29:15 +0530

    trying to resolve deployement issue2

--- controler/user.js
+++ controler/user.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const userModel = require("../model/user");
 const { use } = require("../router/user");
